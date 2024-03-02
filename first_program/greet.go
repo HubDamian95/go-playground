72c5db9b12d0e3178137981ea249260219a0a021
hubdamian95 <hub.d@hotmail.com>
2024-03-02 08:36:27 +0000

    add basics of greet.go

---
+++ first_program/greet.go
@@ -0,0 +1,9 @@
+// go build greet.go -> To build an executable
+// go run greet.go -> to execute without creating executable
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
